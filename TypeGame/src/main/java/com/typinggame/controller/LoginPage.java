64ed4013b6787e84e6d62e43f651603f2cd2b268
wren <11618001@qut.edu.au>
2025-09-10 12:42:21 +1000

    Working on login UI added some resources for it and began the layout

--- TypeGame/src/main/java/com/typinggame/controller/LoginPage.java
+++ TypeGame/src/main/java/com/typinggame/controller/LoginPage.java
@@ -1,4 +1,38 @@
 package com.typinggame.controller;
 
-public class LoginPage {
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+public class LoginPage{
+    @FXML public Button loginButton;
+    public void handleLogin(){
+
+    }
+
+    public void setLoginButton()  {
+
+        Image image = new Image(getClass().getResourceAsStream("Button.png"));
+        ImageView imageView = new ImageView(image);
+
+        loginButton.setText("Login");
+        loginButton.setStyle("-fx-background-color: transparent;");
+        loginButton.setGraphic(imageView);
+
+
+        // Add an event click to the button
+        loginButton.setOnAction(new EventHandler<ActionEvent>(){
+            @Override
+            public void handle(ActionEvent event) {
+                // Input logic for handling the event
+            }
+        });
+
+    }
 }
+
+
